c724b4116f8bfbc17fbaa6e17eaaec63a5c90db8
Ivan Smahlenko <ivan_smahlenko@epam.com>
2023-01-04 00:52:54 +0200

    fix: reverted name configuration for datasets in main.tf due to terraform error

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "google_bigquery_dataset" "main" {
   for_each = var.dataset_name
 
-  dataset_id                 = replace("${var.env}-${var.product_base_name}-${each.key}", "_", "-")
+  dataset_id                 = replace("${var.env}-${var.product_base_name}-${each.key}", "-", "_")
   location                   = var.location
   delete_contents_on_destroy = var.delete_data
 
